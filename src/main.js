0a19dc2577bf5580ece79e9672c903ff653932c3
Nathannie Gomes <nathannieg@gmail.com>
2022-10-20 21:53:21 -0300

    update project

--- src/main.js
+++ src/main.js
@@ -1,1 +1,146 @@
 import "./css/index.css"
+import IMask from "imask"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+const ccHolder = document.querySelector("#card-holder")
+const ccHolderOnCard = document.querySelector(".cc-holder .value")
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#436D99", "#2D57F2"],
+    mastercard: ["#DF6F29", "#C69347"],
+    elo: ["#0258FF", "#D7EA00"],
+    default: ["black", "gray"]
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+// globalThis ou window deixa a função global, o que permite executá-la direto do console (globalThis.setCardType("mastercard"))
+globalThis.setCardType = setCardType
+
+// Máscara para o CVC (4 dígitos)
+const securityCode = document.querySelector("#security-code")
+
+const securityCodePattern = {
+  mask: "0000"
+}
+
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+// Máscara para expiração
+const expirationDate = document.querySelector("#expiration-date")
+
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2)
+    }
+  }
+}
+
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+// Máscara para cartão (dynamic mask)
+
+const cardNumber = document.querySelector("#card-number")
+
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: "mastercard"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex:
+        /^4011(78|79)|^43(1274|8935)|^45(1416|7393|763(1|2))|^50(4175|6699|67[0-6][0-9]|677[0-8]|9[0-8][0-9]{2}|99[0-8][0-9]|999[0-9])|^627780|^63(6297|6368|6369)|^65(0(0(3([1-3]|[5-9])|4([0-9])|5[0-1])|4(0[5-9]|[1-3][0-9]|8[5-9]|9[0-9])|5([0-2][0-9]|3[0-8]|4[1-9]|[5-8][0-9]|9[0-8])|7(0[0-9]|1[0-8]|2[0-7])|9(0[1-9]|[1-6][0-9]|7[0-8]))|16(5[2-9]|[6-7][0-9])|50(0[0-9]|1[0-9]|2[1-9]|[3-4][0-9]|5[0-8]))/,
+      cardtype: "elo"
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default"
+    }
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+      return number.match(item.regex)
+    })
+
+    //console.log(foundMask)
+    return foundMask
+  }
+}
+
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+const addButton = document.querySelector("#add-card")
+
+addButton.addEventListener("click", () => {
+  alert("Cartão adicionado.")
+})
+
+document.querySelector("form").addEventListener("submit", e => {
+  e.preventDefault()
+})
+
+// Capturando nome e exibindo no cartão.
+ccHolder.addEventListener("input", () => {
+  ccHolderOnCard.innerText =
+    ccHolder.value.length === 0 ? "NOME SOBRENOME" : ccHolder.value
+})
+
+// Eventos do iMask | Exibindo o CCV no cartão
+securityCodeMasked.on("accept", () => {
+  updateSecurityCode(securityCodeMasked.value)
+})
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector(".cc-security .value")
+
+  ccSecurity.innerText = code.length === 0 ? "000" : code
+}
+
+// Eventos do iMask | Exibindo número do cartão
+cardNumberMasked.on("accept", () => {
+  const cardType = cardNumberMasked.masked.currentMask.cardtype
+  setCardType(cardType)
+  updateCardNumber(cardNumberMasked.value)
+})
+
+function updateCardNumber(number) {
+  const ccNumber = document.querySelector(".cc-number")
+
+  ccNumber.innerText = number.length === 0 ? "0000 0000 0000 0000" : number
+}
+
+// Eventos do iMask | Exibindo data de expiração no cartão
+expirationDateMasked.on("accept", () => {
+  updateExpirationDate(expirationDateMasked.value)
+})
+
+function updateExpirationDate(date) {
+  const ccExpiration = document.querySelector(".cc-expiration .value")
+
+  ccExpiration.innerText = date.length === 0 ? "MM/AA" : date
+}
